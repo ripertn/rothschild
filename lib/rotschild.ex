f9ab6394760055e7e28822dbb530f81b30b7aaf9
Nathan <nathan.ripert@kbrwadventure.com>
2018-11-29 22:03:02 +0100

    add all basic files

---
+++ lib/rotschild.ex
@@ -0,0 +1,18 @@
+defmodule Rotschild do
+  @moduledoc """
+  Documentation for Rotschild.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rotschild.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rotschild_test.exs
@@ -0,0 +1,8 @@
+defmodule RotschildTest do
+  use ExUnit.Case
+  doctest Rotschild
+
+  test "greets the world" do
+    assert Rotschild.hello() == :world
+  end
+end
